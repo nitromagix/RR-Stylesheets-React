d73d95954c81e1088a7c126c788b860f4e284fa1
Martin <nitromagix@gmail.com>
2022-07-09 19:43:22 -0700

    refactored code

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -4,9 +4,9 @@ import { Fragment } from "react";
 
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
-import trace from "../helper"
+import trace from "../helper";
 
-const TAB_LOCAL_STORAGE_NAME = "mxjkbvlsih";
+const CURRENT_TAB_LS_NAME = "mxjkbvlsih";
 
 function Tabs() {
    window.onpopstate = (e) => {
@@ -17,7 +17,7 @@ function Tabs() {
    const navItemClick = (e) => {
       const pathname = e.target.pathname;
       setCurrentTab(pathname);
-   }
+   };
 
    const setCurrentTab = (pathname) => {
       const previousTab = document.querySelector(".nav-item>a.nav-link.active");
@@ -27,11 +27,11 @@ function Tabs() {
       );
       activeTab.classList.add("active");
 
-      localStorage.setItem(TAB_LOCAL_STORAGE_NAME, pathname);
+      localStorage.setItem(CURRENT_TAB_LS_NAME, pathname);
    };
 
    const getCurrentTab = () => {
-      let tab = localStorage.getItem(TAB_LOCAL_STORAGE_NAME);
+      let tab = localStorage.getItem(CURRENT_TAB_LS_NAME);
       return tab ? tab : document.location.pathname;
    };
 
@@ -47,32 +47,17 @@ function Tabs() {
                <Nav.Link eventKey="disabled" disabled></Nav.Link>
             </Nav.Item>
             <Nav.Item>
-               <Nav.Link
-                  as={Link}
-                  to="/"
-                  eventKey="/"
-                  onClick={navItemClick}
-               >
+               <Nav.Link as={Link} to="/" eventKey="/" onClick={navItemClick}>
                   Home
                </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-               <Nav.Link
-                  as={Link}
-                  to="/1"
-                  eventKey="/1"
-                  onClick={navItemClick}
-               >
+               <Nav.Link as={Link} to="/1" eventKey="/1" onClick={navItemClick}>
                   Component 1
                </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-               <Nav.Link
-                  as={Link}
-                  to="/2"
-                  eventKey="/2"
-                  onClick={navItemClick}
-               >
+               <Nav.Link as={Link} to="/2" eventKey="/2" onClick={navItemClick}>
                   Component 2
                </Nav.Link>
             </Nav.Item>
